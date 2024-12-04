dc695c78fd30a58bcc2f76d797bb1fa417f4b678
Abubakrj0n <big500530@gmail.com>
2024-12-04 18:58:47 +0500

    Add files via upload

---
+++ Home work 11/Home work 11/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int A = Convert.ToInt32(Console.ReadLine()); 
+int B = Convert.ToInt32(Console.ReadLine());
+for (int i = B; i > A; i--)
+{
+    Console.WriteLine(i); 
+}
+        
